fa1826186aaa2afa5dabb057884843f3da5961ca
biswa-ganguly <biswaganguly16@gmail.com>
2024-07-01 21:41:07 +0530

    connecting

--- app.js
+++ app.js
@@ -13,13 +13,38 @@ const io = socket(server)
 const chess = new Chess()
 
 let players = {}
-let currentPlayer = "W"
+let currentPlayer = "w"
 
 app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname, "public")))
 
 app.get("/",(req,res)=>{
     res.render("index", {title: "Chess Game"})
+})
+
+io.on("connection", function(uniquesocket){
+    console.log("New connection made")
+
+    if(!players.white){
+        players.white = uniquesocket.id
+        uniquesocket.emit("playerRole", "w")
+    }
+    else if(!players.black){
+        players.black = uniquesocket.id
+        uniquesocket.emit("playerRole", "b")
+    }
+    else{
+        uniquesocket.emit("spectatorRole")
+    }
+
+    uniquesocket.on("disconnect", function(){
+        if(uniquesocket.id === players.white){
+            delete players.white
+        }
+        else if(uniquesocket.id === players.black){
+            delete players.black
+        }
+    })
 })
 
 server.listen(3000,function(){
